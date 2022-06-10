35b476238dccfd5b5e7ccd4c37940ec5047ae30a
Владимир <satana200355s@gmail.com>
2022-06-10 11:36:39 +0600

    test

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -9,7 +9,24 @@ export class Toolbar extends ExcelComponent {
     })
   }
   toHTML() {
-    return '<h1>Toolbar</h1>'
+    return '<div class="button">\n' +
+      '        <span class="material-icons">format_align_left</span>\n' +
+      '      </div>\n' +
+      '      <div class="button">\n' +
+      '        <span class="material-icons">format_align_right</span>\n' +
+      '      </div>\n' +
+      '      <div class="button">\n' +
+      '        <span class="material-icons">format_align_center</span>\n' +
+      '      </div>\n' +
+      '      <div class="button">\n' +
+      '        <span class="material-icons">format_bold</span>\n' +
+      '      </div>\n' +
+      '      <div class="button">\n' +
+      '        <span class="material-icons">format_italic</span>\n' +
+      '      </div>\n' +
+      '      <div class="button">\n' +
+      '        <span class="material-icons">format_underlined</span>\n' +
+      '      </div>'
   }
   onClick(event) {
     console.log(event.target)
